9a2bc82a0702d8f515bcf2d227a15abba3f43ed6
Cassius6826 <1874827080@qq.com>
2026-01-19 23:46:41 +0800

    娣诲姞鏁版嵁鍒嗘瀽鐩稿叧鏂囦欢鍜宒uotai.py

---
+++ duotai.py
@@ -0,0 +1,23 @@
+class Animal:
+
+    def spark(self):
+        print("动物叫")
+
+class Dog(Animal):
+
+    def spark(self):
+        print("汪汪汪")
+
+class Cat(Animal):
+
+    def spark(self):
+        print("喵喵喵")
+
+def make_noise(animal : Animal):
+    animal.spark()
+
+dog = Dog()
+cat = Cat()
+
+make_noise(dog)
+make_noise(cat)
---
+++ 数据分析/file_define.py
@@ -0,0 +1,48 @@
+from data_define import Record
+import json
+
+#定义抽象类
+class FileReader:
+
+    def read_data(self) -> list[Record]:
+        """ 读取文件的数据，读取到的每一条数据都转化为Record对象，将它们封装到list中"""
+        pass
+
+
+class TextFileReader(FileReader):
+
+    def __init__(self, path):
+        self.path = path
+
+    #复写，实现抽象方法
+    def read_data(self) -> list[Record]:
+        f = open(self.path, "r", encoding = "UTF-8")
+
+        record_list = []
+        for line in f.readlines():
+            line = line.strip()     #消除读取到的回车
+            data_list = line.split(",")
+            record = Record(data_list[0], data_list[1], int(data_list[2]), data_list[3])
+            record_list.append(record)
+        
+        f.close()
+        return record_list
+
+
+class JsonFileReader(FileReader):
+    
+    def __init__(self, path):
+        self.path = path
+
+    #复写
+    def read_data(self) -> list[Record]:
+        f = open(self.path, "r", encoding = "UTF-8")
+
+        record_list = []
+        for line in f.readlines():
+            data_dict = json.loads(line)
+            record = Record(data_dict["date"], data_dict["order_id"], int(data_dict["money"]), data_dict["province"])
+            record_list.append(record)
+
+        f.close()
+        return record_list
